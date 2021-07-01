16c17e92ee26741bc7ea46301dbc47bf7274b922
bigoroberto <bigoroberto@gmail.com>
2021-07-01 18:36:49 +0200

    aggiunto il pulsante back

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -6,6 +6,7 @@
         <p>{{ $post->content }}</p>
         <div>
             <a class="btn btn-info" href="{{ route('admin.posts.edit', $post) }}">EDIT</a>
+            <a class="btn btn-success" href="{{ route('admin.posts.index', $post) }}">BACK</a>
         </div>
     </div>
 @endsection
